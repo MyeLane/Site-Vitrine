fb0ba5f17c44670e1022710d0e6ecf112eb3fc08
LEONG Mye Lane Jennifer Kathuicia <45306895+MyeLane@users.noreply.github.com>
2020-08-20 13:37:17 +0300

    formation

--- cfp.php
+++ cfp.php
@@ -10,25 +10,52 @@
     <link rel="stylesheet" href="css/fontawesome-free-5.8.2-web/css/all.min.css">
 </head>
 <body>
+
+    <div id="header" class="main-menu col-12">
+        <nav class="navbar navbar-expand-lg row">
+            <div class="ml-5 col-3">
+                    <img id="LogoMenu" width="50%" src="img/accueil/logo E-media.png" alt="logo">
+            </div>
+            <h4 align="center" class="col-5">Bienvenue - Centre de Formation Professionnelle</h4>
+            <div class="collapse navbar-collapse justify-content-end mr-5 col-3" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#accueil"><i class="fas fa-home mr-2"></i>Accueil</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+
 <div class="row mt-5 col-12">
   <div class="col-3">
     <img src="img/accueil/96735692_559166918357964_1257399878321963008_n.jpg" class="img-fluid" alt="">
     <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
       <a class="nav-link active" id="v-pills-image-tab" data-toggle="pill" href="#v-pills-image" role="tab" aria-controls="v-pills-home" aria-selected="true">IMAGE</a>
+      <span class="solid"></span>
       <a class="nav-link" id="v-pills-son-tab" data-toggle="pill" href="#v-pills-son" role="tab" aria-controls="v-pills-profile" aria-selected="false">SON</a>
+      <span class="solid"></span>
       <a class="nav-link" id="v-pills-web-tab" data-toggle="pill" href="#v-pills-web" role="tab" aria-controls="v-pills-messages" aria-selected="false">WEB</a>
+      <span class="solid"></span>
     </div>
   </div>
   <div class="col-9">
-        <div>
-            <h4 class="text-justify mt-3 p-5">Existante depuis 7 ans, la formation professionnelle a, depuis sa création formée des milliers de jeunes. Elle se tourne vers les jeunes possédant ou non leur baccalauréat désireux d’accéder directement à un métier d’avenir.</h4>
+    <div class="contact-cfp mb-4">
+        <div class="d-flex">
+            <i class="fas ico fa-envelope"></i>
+            <small class="ml-3"> emedia.cfp@gmail.com</small> 
         </div>
+        <div class="d-flex">
+            <i class="fas ico fa-phone"></i>
+            <small class="ml-3"> 033 02 250 86</small> 
+        </div>
+    </div>
     <div class="tab-content mb-5 tab-cfp" id="v-pills-tabContent">
         <div class="tab-pane p-5 fade show active" id="v-pills-image" role="tabpanel" aria-labelledby="v-pills-image-tab">
-            <h2 align="center">FORMATION PROFESSIONNELLE FILIERE IMAGE</h2>
-            <h4>Présentation</h4>
+            <h2 align="center" class="color pb-4 title mb-4">FORMATION PROFESSIONNELLE FILIERE IMAGE</h2>
+            <h4 class="mb-2">Présentation</h4>
             <p class="text-justify">La filière Image a pour but de former des techniciens du métier de l’image. Elle offre une formation essentiellement technique, basé sur les connaissances des bases théoriques et pratiques des techniques et des outils relatifs aux traitements de l’image. La formation octroyée sur un moyen terme, permet aux étudiants d’être directement opérationnel. Face à un environnement professionnel en quête de polyvalence, la formation en Image, se veut être complète, en intégrant tous les modules utiles à l’étudiants. Les étudiants seront aptes à réaliser à traiter les images fixes tels que les affiches, flyers, brochures…en passant par les images mobiles comme les clips, les publicités, les films, documentaires ….ainsi que la production 3D.</p>
-            <h4>Programme</h4>
+            <h4 class="mb-2">Programme</h4>
             <p class="text-justify">D’une durée de 08 Mois, la formation est composée des modules suivantes :</p>
             <ul>
                 <li>Conception cinématographique</li>
@@ -39,22 +66,22 @@
                 <li>Post production</li>
                 <li>3D</li>
             </ul>
-            <h4>Admissibilité</h4>
+            <h4 class="mb-2">Admissibilité</h4>
             <p class="text-justify">La formation est ouverte à toutes personnes, sans limite d’âge, ayant ou non le baccalauréat mais jouissant d’un niveau terminal minimum.</p>
-            <h4>Prérequis</h4>
+            <h4 class="mb-2">Prérequis</h4>
             <p>Connaissances de base en informatique (structure de l’ordinateur…)</p>
-            <h4>Sanction</h4>
+            <h4 class="mb-2">Sanction</h4>
             <p class="text-justify">Un examen officiel composé de partie théorique et pratique, préparé et dirigé par le Ministère de l’Enseignement Technique et de la Formation Professionnelle (METFP) permet d’obtenir un Certificat de Fin de Formation, Spécialité : IMAGE (Equivalent BACCALAUREAT / CATEGORIE III FOP)</p>
-            <h4>Débouchés</h4>
+            <h4 class="mb-2">Débouchés</h4>
             <p class="text-justify">Technicien en audiovisuel : Monteur , Cadreur, Infographiste, Cameraman etc.… au sein des structures tels que les agences de communication, télévision, cinéma, imprimerie, et toutes autres entreprises.</p>
         </div>
         <div class="tab-pane fade p-5" id="v-pills-son" role="tabpanel" aria-labelledby="v-pills-son-tab">
 
 
-            <h2 align="center">FORMATION PROFESSIONNELLE FILIERE SON</h2>
-            <h4>Présentation</h4>
+            <h2 align="center" class="color pb-4 title mb-4">FORMATION PROFESSIONNELLE FILIERE SON</h2>
+            <h4 class="mb-2">Présentation</h4>
             <p class="text-justify">La filière SON a pour objectif de former des techniciens du métier du son. Elle s’adresse aux passionnés du son, qui veulent en faire leur métier et acquérir les bases nécessaires pour évoluer dans ce domaine. Elle permet aux étudiants de maîtriser les matériels ainsi que d’acquérir les techniques nécessaires dans la prise et le traitement de son. Des bases théoriques solides complétés par des pratiques régulières permettent aux étudiants d’être directement opérationnel sur le plan professionnel à l’issu de la formation. Les étudiants seront alors aptes à effectuer une séance d’enregistrement, de faire des compositions, d’arrangement et mixage sur une station audio.</p>
-            <h4>Programme</h4>
+            <h4 class="mb-2">Programme</h4>
             <p class="text-justify">D’une durée de 06 Mois le programme de formation de la filière son se compose :</p>
             <ul>
                 <li>Environnement studio</li>
@@ -64,13 +91,13 @@
                 <li>Mastering</li>
             </ul>
             <p class="text-justify">Un studio son muni de tous les matériels nécessaires est mise à disposition pour parfaire la formation.</p>
-            <h4>Admissibilité</h4>
+            <h4 class="mb-2">Admissibilité</h4>
             <p class="text-justify">La formation est ouverte à toutes personnes, sans limite d’âge, ayant ou non le baccalauréat mais jouissant d’un niveau terminal minimum.</p>
-            <h4>Prérequis</h4>
+            <h4 class="mb-2">Prérequis</h4>
             <p>Connaissances de base en informatique (structure de l’ordinateur…)</p>
-            <h4>Sanction</h4>
+            <h4 class="mb-2">Sanction</h4>
             <p class="text-justify">Un examen officiel composé de partie théorique et pratique, préparé et dirigé par le Ministère de l’Enseignement Technique et de la Formation Professionnelle (METFP) permet d’obtenir un Certificat de Fin de Formation, Spécialité : SON (Equivalent BACCALAUREAT / CATEGORIE III FOP)</p>
-            <h4>Débouchés</h4>
+            <h4 class="mb-2">Débouchés</h4>
             <p class="text-justify">Technicien son : Opérateur son, Sonorisateur, Sound designer, Production musicale.… au sein des structures tels que les studios de sonorisation, radio.</p>
 
       
@@ -78,10 +105,10 @@
         <div class="tab-pane fade p-5" id="v-pills-web" role="tabpanel" aria-labelledby="v-pills-web-tab">
 
 
-        <h2 align="center">FORMATION PROFESSIONNELLE FILIERE WEB</h2>
-            <h4>Présentation</h4>
+        <h2 align="center" class="color title pb-4 mb-4">FORMATION PROFESSIONNELLE FILIERE WEB</h2>
+            <h4 class="mb-2">Présentation</h4>
             <p class="text-justify">La filière WEB a pour objectif de former des techniciens en web. Elle est destinée à tous ceux qui souhaitent avoir les compétences nécessaires pour la conception et le développement d’un site web. La formation basée sur les connaissances aussi bien théoriques que pratiques permet aux étudiants qui la suivent d’avoir toutes les compétences nécessaires pour intégrer le monde professionnel. Les étudiants seront en mesure de créer un site web, et de maitriser toutes les fonctionnalités pour son développement.</p>
-            <h4>Contenu</h4>
+            <h4 class="mb-2">Contenu</h4>
             <p class="text-justify">D’une durée de 06 Mois le programme de formation de la filière web se compose :</p>
             <ul>
                 <li>HTML</li>
@@ -90,13 +117,13 @@
                 <li>PHP</li>
                 <li>PAO</li>
             </ul>
-            <h4>Admissibilité</h4>
+            <h4 class="mb-2">Admissibilité</h4>
             <p class="text-justify">La formation est ouverte à toutes personnes, sans limite d’âge, ayant ou non le baccalauréat mais jouissant d’un niveau terminal minimum.</p>
-            <h4>Prérequis</h4>
+            <h4 class="mb-2">Prérequis</h4>
             <p>Connaissances de base en informatique (structure de l’ordinateur…)</p>
-            <h4>Sanction</h4>
+            <h4 class="mb-2">Sanction</h4>
             <p class="text-justify">Un examen officiel composé de partie théorique et pratique, préparé et dirigé par le Ministère de l’Enseignement Technique et de la Formation Professionnelle (METFP) permet d’obtenir un Certificat de Fin de Formation, Spécialité : SON (Equivalent BACCALAUREAT / CATEGORIE III FOP)</p>
-            <h4>Débouchés</h4>
+            <h4 class="mb-2">Débouchés</h4>
             <p class="text-justify">Technicien en web : webmaster, webdesigner, .… au sein des structures telles que les agences de communication, projet web et toutes autres entreprises.</p>
 
 
--- online.php
+++ online.php
@@ -12,6 +12,115 @@
 <body>
     
 
+
+<div id="header" class="main-menu col-12">
+        <nav class="navbar navbar-expand-lg row">
+            <div class="ml-5 col-3">
+                    <img id="LogoMenu" width="50%" src="img/accueil/logo E-media.png" alt="logo">
+            </div>
+            <h4 align="center" class="col-5">Bienvenue - Université (formation présentielle)</h4>
+            <div class="collapse navbar-collapse justify-content-end mr-5 col-3" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#accueil"><i class="fas fa-home mr-2"></i>Accueil</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+
+<div class="row mt-5 col-12">
+  <div class="col-3">
+    <img src="img/accueil/96537171_797577050775702_7714680588480806912_n.jpg" class="img-fluid" alt="">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <a class="nav-link active" id="v-pills-image-tab" data-toggle="pill" href="#v-pills-image" role="tab" aria-controls="v-pills-home" aria-selected="true">Informatique et Télécommunication</a>
+      <span class="solid"></span>
+      <a class="nav-link" id="v-pills-son-tab" data-toggle="pill" href="#v-pills-son" role="tab" aria-controls="v-pills-profile" aria-selected="false">Communication Audiovisuelle et Numérique</a>
+      <span class="solid"></span>
+      <a class="nav-link" id="v-pills-web-tab" data-toggle="pill" href="#v-pills-web" role="tab" aria-controls="v-pills-messages" aria-selected="false">Marketing Publicité et Journalisme</a>
+      <span class="solid"></span>
+    </div>
+  </div>
+  <div class="col-9">
+    <div class="contact-cfp mb-4">
+        <div class="d-flex">
+            <i class="fas ico fa-envelope"></i>
+            <small class="ml-3">scolarite.universite.emedia@gmail.com</small> 
+        </div>
+        <div class="d-flex">
+            <i class="fas ico fa-phone"></i>
+            <small class="ml-3"> 034 14 518 75</small> 
+        </div>
+    </div>
+    <div class="tab-content mb-5 tab-cfp" id="v-pills-tabContent">
+        <div class="tab-pane p-5 fade show active" id="v-pills-image" role="tabpanel" aria-labelledby="v-pills-image-tab">
+            <h2 align="center" class="color title mb-4 pb-4">FILIERE TECHNOLOGIES DE L’INFORMATIQUE ET DE LA COMMUNICATION (TIC) - LICENCE</h2>
+            <h4 class="mb-2">Objectifs</h4>
+            <p class="text-justify">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+            <h4 class="mb-2">Contenu de la formation</h4>
+            <p class="text-justify">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+            <h4 class="mb-2">Parcours :</h4>
+            <ul>
+                <li>Réseau et Système</li>
+                <li>Télécommunication</li>
+                <li>Génie Logiciel</li>
+            </ul>
+            <h4 class="mb-2">Admissibilité</h4>
+            <p class="text-justify">La formation est ouverte à toutes personnes ayant un baccalauréat des séries scientifiques (C/D).</p>
+            <h4 class="mb-2">Sanction</h4>
+            <p class="text-justify">La formation permet aux étudiants d’acquérir les diplômes de Licence en TECHNOLOGIE DE L’INFORMATIQUE ET DE LA TELECOMMUNICATION et ce suivant le système LMD actuellement appliqué à Madagascar.</p>
+            <h4 class="mb-2">Débouchés</h4>
+            <p class="text-justify">Les étudiants seront apte à exercer dans toutes les organisations utilisant les nouvelles technologies de l'information et de la communication (TIC) en tant qu’administrateur Systèmes et Réseaux, Responsable d’Exploitation, …</p>
+        </div>
+        <div class="tab-pane fade p-5" id="v-pills-son" role="tabpanel" aria-labelledby="v-pills-son-tab">
+
+
+            <h2 align="center" class="color title pb-4 mb-4">FILIERE COMMUNICATION AUDIOVISUELLE ET NUMERIQUE (CAN) - LICENCE</h2>
+            <h4 class="mb-2">Objectifs</h4>
+            <p class="text-justify">La mention Communication audiovisuelle et infographie, accessible à tous les bacheliers, permet à ses étudiants d'acquérir de solides connaissances théoriques dans le domaine de l'audiovisuel. Elle forme les futurs professionnels des métiers artistiques et techniques de l'audiovisuel. Destiné aux passionnés de l’audiovisuel, elle permet aux étudiants d’avoir les capacités nécessaires pour initier et accompagner la création d'œuvres audiovisuelles, tout en maitrisant parfaitement les concepts de la communication.</p>
+            <h4 class="mb-2">Contenu de la formation</h4>
+            <p class="text-justify">Reposant sur le système LMD, en vigueur à Madagascar, la formation comprend les matières ci-après : Infographie 2D/3D, Son , Atelier photographie, Atelier documentaire, Base de la programmation, Post production, Direction d’acteur, Theorie de l’Information et de la Communication, Psychologie de la communication, Expression artistique, Gestion de projet….</p>
+            <h4 class="mb-2">Parcours :</h4>
+            <ul>
+                <li>Image</li>
+                <li>Développement web</li>
+            </ul>
+            <h4 class="mb-2">Admissibilité</h4>
+            <p class="text-justify">La formation est ouverte à toutes personnes ayant un baccalauréat, de toutes les séries : A/C/D/Technique</p>
+            <h4 class="mb-2">Sanction :</h4>
+            <p>La formation permet aux étudiants d’acquérir les diplômes de Licence en COMMUNICATION AUDIOVISUELLE ET NUMERIQUE, et ce suivant le système LMD actuellement appliqué à Madagascar.</p>
+            <h4 class="mb-2">Débouchés</h4>
+            <p class="text-justify">La formation permet d’occuper des postes de responsable de développement de projets, responsable des coproductions …. dans des entreprises telles que les sociétés de production, les sociétés de distribution, les chaînes de télévision, sites Internet, Industrie cinématographique….</p>
+
+      
+        </div>
+        <div class="tab-pane fade p-5" id="v-pills-web" role="tabpanel" aria-labelledby="v-pills-web-tab">
+
+
+        <h2 align="center" class="color title pb-4 mb-4">FILIERE MARKETING, PUBLICITE ET JOURNALISME (MPJ) - LICENCE</h2>
+            <h4 class="mb-2">Objectifs</h4>
+            <p class="text-justify">La formation s’adresse aux passionnées de la communication. Elle permet de devenir un professionnel qui maîtrise les méthodes et les outils de communication, de marketing et surtout les réalisations publicitaires, susceptible de réaliser en amont des études sur les différents marchés, mais aussi élaborer les supports et moyens publicitaires pertinents, Elle est combinée avec un cursus de journalisme qui permet à l’étudiant de maîtriser tous les aspects de ce domaine.</p>
+            <h4 class="mb-2">Contenu de la formation</h4>
+            <p class="text-justify">La formation en Marketing, Publicité et Journalisme regroupe entre autre les matières suivantes : Fondamentaux de la communication, Communication Visuelle, PAO, Culture Générale, Théorie du journalisme, Technique de son, Technique Vidéo, Télévision, Radio, Multimédia, Atelier d’écritures, Marketing.</p>
+            <h4 class="mb-2">Parcours </h4>
+            <ul>
+                <li>Marketing Publicité</li>
+                <li>Journalisme</li>
+            </ul>
+            <h4 class="mb-2">Admissibilité</h4>
+            <p class="text-justify">La formation est ouverte à toutes personnes ayant un baccalauréat, de toutes les séries : A/C/D/Technique</p>
+            <h4 class="mb-2">Sanction</h4>
+            <p class="text-justify">La formation permet aux étudiants d’acquérir les diplômes de Licence en MARKETING PUBLICITÉ ET JOURNALISME, et ce suivant le système LMD actuellement appliqué à Madagascar.</p>
+            <h4 class="mb-2">Débouchés</h4>
+            <p class="text-justify">Les étudiants issus de la formation pourront exercer dans toutes organisations en tant que chef de publicité, responsable marketing, consultant, chef de projet, concepteur-rédacteur, directeur artistique, et journaliste.</p>
+
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
     <script src="js/jquery-3.3.1.slim.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
