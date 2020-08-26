d0bfc290fefeb551bc98723dbc4e4dd3dae302ec
santatra22front <santatra22ratsitohaina@gmail.com>
2020-08-26 13:38:57 +0300

    responsive1.0

--- index.php
+++ index.php
@@ -19,33 +19,37 @@ function dateToFrench($date, $format)
     <title>E-media</title>
     <link rel="icon" type="icon" href="img/logo E-media copie.png">
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
     <link rel="stylesheet" href="css/fontawesome-free-5.8.2-web/css/all.min.css">
     <!--<link rel="stylesheet" href="css/aos.css">-->
+    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
 </head>
 
 <body>
 
     <!--https://www.youtube.com/watch?v=MubsuxA72yU-->
     <section>
-        <div class="container-fluid mt-5">
-            <div class="row banner ml-5 mt-5">
-                <div class="col-7 marge mt-5">
-                    <h1 class="">Université <span class="banner-media">E-media</span></h1>
-                    <h4>Visons haut et loin</h4>
-                    <p class="col-10 banner-text ml-1">Notre vision c’est de bâtir l’Afrique de demain, donner à tout et en chacun une formation Universitaire de haut niveau mais à moindre cout, et avec plusieurs spécialisations au choix.</p>
-                    <div class="mt-4">
-                        <button type="button" class="btn btn-info">Contact</button>
-                        <button type="button" class="btn ml-2 btn-outline-info">Se connecter</button>
-                    </div>
-                    <div class="social mt-4 p-2" align="center">
-                        <a href=""><i class="fab fa-facebook-square"></i></a>
-                        <a href="" class="ml-2"><i class="fab fa-linkedin"></i></a>
-                        <a href="" class="ml-2"><i class="fab fa-twitter-square"></i></a>    
-                    </div>
+        <div class="container-fluid mt-5 mb-lg-5">
+            <div class="row banner ml-2 ml-sm-2 ml-md-5 ml-lg-5 mt-5 mb-lg-5">
+                <div class="col-sm-12 col-md-12 col-lg-7 col-xl-7 marge mb-lg-5 mt-lg-5">
+                   <div class="row">
+                        <h1 class="col-12 animated bounceInRight" style="animation-delay: 1s">Université <span class="banner-media"> E-media</span></h1>
+                        <h4 class="animated animated bounceInLeft" style="animation-delay: 2s">Visons haut et loin</h4>
+                        <p class="col-10 col-sm-10 col-md-10 col-lg-12 col-xl-10 banner-text ml-1 animated bounceInDown" style="animation-delay: 3s;">Notre vision c’est de bâtir l’Afrique de demain, donner à tout et en chacun une formation Universitaire de haut niveau mais à moindre cout, et avec plusieurs spécialisations au choix.</p>
+                        <div class="col-12 mt-4 d-sm-block d-md-flex d-lg-flex">
+                            <button type="button" class="btn mt-2 btn-info animated zoomInLeft" style="animation-delay: 3s;">Contact</button>
+                            <button type="button" class="btn mt-2 ml-lg-3 btn-outline-info animated zoomInRight" style="animation-delay: 3s;">Se connecter</button>
+                        </div>
+                        <div class="col-5 col-sm-3 col-md-3 col-lg-3 social ml-3 mt-4 mb-sm-5 p-2 text-center animated zoomIn" style="animation-delay: 4s;">
+                            <a href=""><i class="fab fa-facebook-square"></i></a>
+                            <a href="" class="ml-2"><i class="fab fa-linkedin"></i></a>
+                            <a href="" class="ml-2"><i class="fab fa-twitter-square"></i></a>    
+                        </div>           
+                   </div>
                 </div>
-                <div class="col-5 test mb-2">
+                
+                <div class="col-5 test animated zoomIn" style="animation-delay: 2s;">
                     
                 </div>
             </div>
@@ -65,8 +69,9 @@ function dateToFrench($date, $format)
             <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item mr-2"><a class="nav-link" href="#accueil">Accueil<span class="bottom-solid"></span></a></li>
-                    <li class="nav-item mr-2"><a class="nav-link" href="#Actualite">Actualité<span class="bottom-solid"></span></a></li>
+                    <li class="nav-item mr-2"><a class="nav-link" href="#actualite">Actualité<span class="bottom-solid"></span></a></li>
                     <li class="nav-item mr-2"><a class="nav-link" href="#formation">Formation<span class="bottom-solid"></span></a></li>
+                    <li class="nav-item mr-2"><a class="nav-link" href="#objectif">Objectif<span class="bottom-solid"></span></a></li>
                     <li class="nav-item mr-2"><a class="nav-link" href="#contact">Contact<span class="bottom-solid"></span></a></li>
                     <li class="nav-item ml-2 nav-seconnecter" data-tooltip="Se connecter"><a class="nav-link" href="#lien"><i class="fa fa-user"></i></a></li>
                 </ul>
@@ -77,43 +82,43 @@ function dateToFrench($date, $format)
     <div class="container-fluid contenu">
 
         <!--apropos-->
-        <div class="row apropos mt-3 ml-5">
+        <div class="row apropos mt-3 ml-2 ml-lg-5" id="accueil">
             <div class="col-4 apropos-left mt-5 aos-init aos-animate code code--small code--up" data-aos="fade-up" data-aos-duration="2000">
                 <img src="img/accueil/apropos2.jpg" alt="" class="img-fluid mt-5">
             </div>
 
-            <div class="col-8 apropos-right">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-8  apropos-right">
                 <h1 class="ml-5 mt-5 aos-init aos-animate code code--small code--left" data-aos="zoom-out" data-aos-duration="2000">A propos de l'<span class="banner-media">E-media</span></h1>
                 <div class="row">
-                    <div class="cree col-6 mt-4 aos-init aos-animate code code--small code--left" data-aos="zoom-in" data-aos-duration="2000">
+                    <div class="cree col-10 col-sm-10 col-md-6 col-lg-6 mt-5 aos-init aos-animate code code--small code--left" data-aos="zoom-in" data-aos-duration="2000">
                         <span>Crée en 2011, E-MEDIA est actuellement un établissement de référence et un acteur incontournable dans le domaine de la formation à Madagascar et notamment en Afrique francophone.
                          Il s’oriente dans plusieurs domaines et spécialisations</span>
                     </div>
-                    <div class="col-6 mt-5 aos-init aos-animate code code--small code--left" data-aos="fade-up-left" data-aos-duration="2000">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-6 mt-5 aos-init aos-animate code code--small code--left" data-aos="fade-up-left" data-aos-duration="2000">
                         <img src="img/accueil/apropos1.jpg" alt="" class="img-fluid">
                     </div>
-                    <div class="col-6 ml-5 aos-init aos-animate code code--small code--right" data-aos="fade-up-right" data-aos-duration="2000">
-                        <img src="img/accueil/apropos1.jpg" alt="" class="img-fluid oui ml-5">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-6 ml-md-5 ml-lg-5 aos-init aos-animate code code--small code--right" data-aos="fade-up-right" data-aos-duration="2000">
+                        <img src="img/accueil/apropos1.jpg" alt="" class="img-fluid oui ml-md-5 ml-lg-5">
                     </div>
                 </div>
             </div>
         </div>
 
         <!--actulites-->
-        <div class="row actualites mt-5">
-            <div class="col-8">
+        <div class="row actualites mt-5" id="actualite">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-8">
                 <div class="row card-deck">
-                    <div class="col-12 title ml-5 mt-5">
+                    <div class="col-12 actualites ml-5 mt-5">
                         <h1 class="mt-4"><span class="banner-media">Nos</span> actualités</h1>
                     </div>
-                    <div class="card col-6 mt-5">
+                    <div class="card col-12 col-sm-12 col-md-6 col-lg-6 mt-5">
                         <img class="card-img-top" src="img/accueil/emedia pour site 2.jpg" class="img-fluid" alt="universite">
                         <div class="card-body">
                             <h5 class="card-title">Formation Universitaire en ligne</h5>
                             <small class="card-text">La formation en ligne de l'Université E-media est maintenant ouverte pour tous les pays d’Afrique francophones.</small>
                         </div>
                     </div>
-                    <div class="card col-6 mt-5">
+                    <div class="card col-12 col-sm-12 col-md-6 col-lg-6 mt-5">
                         <img class="card-img-top" src="img/accueil/alefa sita-01.jpg" class="img-fluid" alt="universite">
                         <div class="card-body">
                             <h5 class="card-title">Formation baccalauréat en ligne</h5>
@@ -124,7 +129,7 @@ function dateToFrench($date, $format)
             </div>
 
             <!--google news-->
-            <div class="col-4 google-news mt-5">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 google-news mt-5">
                 <div class="row mt-5">
                     <ul class="nav nav-pills col-12 nav-justified mb-3" id="pills-tab" role="tablist">
                         <li class="nav-item" role="presentation">
@@ -230,22 +235,22 @@ function dateToFrench($date, $format)
             </div>
         </div>
 
-        <!--Nos frmations-->
-        <div class="formation mt-5">
+        <!--Nos formations-->
+        <div class="formation mt-5" id="formation">
             <h1 class="ml-5"><span class="banner-media">Nos</span> formation</h1>
             <div class="">
                 <img class="choisir img-fluid" src="img/accueil/71382219_1113234622208342_1065529829350178816_o.jpg" alt="">
             </div>
-            <div class="container">
+            <div class="container-fluid">
                 <div class="row">
 
-                    <div class="col-4" align="center">
-                        <div class=" pres col-9">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-4" align="center">
+                        <div class=" pres col-10 col-sm-10 col-md-10 col-lg-10">
                             <div class="icon-formation">
                                 <i class="mt-5 fas fa-user-graduate"></i>
                             </div>
                             <div class="mt-5">
-                                <h3 class="card-titre">Université en Présentielle</h3>
+                                <h3 class="card-titre">Université Présentielle</h3>
                                 <div class="mt-3 savoir-plus">
                                     <a class="" href="#">En savoir plus</a>
                                 </div>
@@ -253,8 +258,8 @@ function dateToFrench($date, $format)
                         </div>
                     </div>
 
-                    <div class="col-4" align="center">
-                        <div class="ligne col-9">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-4" align="center">
+                        <div class="ligne col-10 col-sm-10 col-md-10 col-lg-10">
                             <div class="icon-formation">
                                 <i class="mt-5 fab fa-audible"></i>
                             </div>
@@ -268,8 +273,8 @@ function dateToFrench($date, $format)
                         </div>
                     </div>
 
-                    <div class="col-4" align="center">
-                        <div class="cfp col-9">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-4" align="center">
+                        <div class="cfp col-10 col-sm-10 col-md-10 col-lg-10">
                             <div class="icon-formation">
                                 <i class="mt-5 fas fa-graduation-cap"></i>
                             </div>
@@ -302,7 +307,7 @@ function dateToFrench($date, $format)
         </div>
 
         <!--Nos objectifs-->
-        <div class="objectifs container-fluid mt-5">
+        <div class="objectifs container-fluid mt-5" id="objectif">
             <h1 align="center"><span class="banner-media">Nos</span> objectifs</h1>
 
             <div class="row mt-3">
